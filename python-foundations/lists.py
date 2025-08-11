e174b83cf6c5e202a8fc9b1355e676f3e22b693c
alexanderplz <michaelquiban@proton.me>
2025-08-10 17:51:46 -0700

    practicing with lists and different data types in lists

---
+++ python-foundations/lists.py
@@ -0,0 +1,8 @@
+# I'm going to make a list containing my favorite things.
+# Lists are a compound data type that can group values together.
+
+favorite_things = ["Robotics", "Python", "Finance", "Reading", "Working Out"]
+print("My favorite things are " , favorite_things)
+
+my_favorite_number = ["My favorite number is", 7, True]
+print(my_favorite_number)
