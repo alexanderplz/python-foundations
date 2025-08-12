03380413fb15d9294629a93a5ad8180c995d52aa
alexanderplz <michaelquiban@proton.me>
2025-08-11 20:51:30 -0700

    small use of np.array() to a list of weights from customers

--- python-foundations/numpy.py
+++ python-foundations/numpy.py
@@ -1,1 +1,9 @@
-import numpy
+import numpy as np
+
+#numpy is a powerful library for numerical computations in Python.
+personal_trainer_weights = [185, 225, 275, 315, 365, 405]
+
+np_personal_trainer_weights = np.array(personal_trainer_weights)
+
+#print the numpy array
+print("My personal trainer's weights in a numpy array: ", np_personal_trainer_weights)
